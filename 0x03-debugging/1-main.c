885b29b31e69d56452d974cf29abeb5e7536002d
Clarenced1 <clarencegreat@gmail.com>
2022-12-17 00:00:59 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,11 +3,9 @@
 /**
  * main - causes an infinite loop
  * Return: 0
- *
  */
 
 int main(void)
-
 {
 	int i;
 
@@ -15,13 +13,13 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)*/
+	/*
+	 * while (i < 10)
+	*{
+	* putchar(i);
+	*}
+	*/
 
-	/* {*/
-
-	/* putchar(i);*/
-
-	/* }*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
