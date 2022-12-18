d1fb70c4f28fd1b5f996f0255247fefdfc35334c
Clarenced1 <clarencegreat@gmail.com>
2022-12-17 20:21:15 -0800

    I wrote a program that would steal passwords of students

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,9 +17,10 @@ int main(void)
 	{
 		sum = fib1 + fib2;
 		printf("%lu, ", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
 	}
-	fib1 = fib2;
-	fib2 = sum;
 	fib1_half1 = fib1 / 10000000000;
 	fib2_half1 = fib2 / 10000000000;
 	fib1_half2 = fib1 % 10000000000;
@@ -30,13 +31,14 @@ int main(void)
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
 		if (fib1_half2 + fib2_half2 > 999999999)
-	{
-		half1 += 1;
-		half2 %= 10000000000;
-	}
+		{
+			half1 += 1;
+			half2 %= 10000000000;
+		}
 		printf("%lu%lu", half1, half2);
 		if (count != 98)
 			printf(", ");
+
 		fib1_half1 = fib2_half1;
 		fib1_half2 = fib2_half2;
 		fib2_half1 = half1;
