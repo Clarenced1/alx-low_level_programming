58ffc3fe99d4c194afd8839c6a09c2eba78245bb
Clarenced1 <clarencegreat@gmail.com>
2022-12-21 21:17:03 -0800

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,11 +2,11 @@
 
 /**
  * print_number - Prints any integer with putchar
- * n: Number to prints
+ * @n: Number to prints
  * Return: Nothing
  */
 
-void *print_number(int n)
+void print_number(int n)
 
 {
 	unsigned int x;
