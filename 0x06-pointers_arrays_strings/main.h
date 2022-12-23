275784196c781614bc7578f014ed4cd44fe193b5
Clarenced1 <clarencegreat@gmail.com>
2022-12-23 13:26:00 -0800

    Task 10

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int main(void);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -26,7 +28,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 
 int _putchar(char c);
-
-int main(void);
 
 #endif
